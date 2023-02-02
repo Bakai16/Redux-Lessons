b2574e115dc3ac50a0f4d1cb07a40e3ff4993432
Bakai <bakayakylbekov3@gmail.com>
2023-02-02 15:28:33 +0600

    193. Соединяем React и Redux при помощи connect

--- src/index.js
+++ src/index.js
@@ -2,41 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
-import * as action from './actions'
+import { Provider } from 'react-redux';
 
-
+import App from './components/App';
 
 const store = createStore(reducer)
 
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-    document.getElementById('counter').textContent =  getState().value;
-}
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(action, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc)
-
-document.getElementById('dec').addEventListener('click', dec)
-
-document.getElementById('rnd').addEventListener('click', () =>{
-    const value = Math.floor(Math.random() * 10); 
-    rnd(value);
-})
-
-dispatch({type: 'INC'});
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
+</React.StrictMode>
+);  
 
-  </React.StrictMode>
-);
- 
+
+
+
+
+
+
+
+
