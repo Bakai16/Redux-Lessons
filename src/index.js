976717334b59c3490a286c55efbffee5d77d07cd
Bakai <bakayakylbekov3@gmail.com>
2023-02-02 13:55:23 +0600

    191. Оптимизация через actionCreators и bindActionCreator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import * as action from './actions'
+
+
+
+const store = createStore(reducer)
+
+const {dispatch, subscribe, getState} = store;
+
+const update = () => {
+    document.getElementById('counter').textContent =  getState().value;
+}
+subscribe(update);
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//     dispatch(creator(...args));
+// }
+
+const {inc, dec, rnd} = bindActionCreators(action, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc)
+
+document.getElementById('dec').addEventListener('click', dec)
+
+document.getElementById('rnd').addEventListener('click', () =>{
+    const value = Math.floor(Math.random() * 10); 
+    rnd(value);
+})
+
+dispatch({type: 'INC'});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
